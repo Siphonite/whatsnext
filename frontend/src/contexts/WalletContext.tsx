e53a24ab363e5090ed7303f2574460c542ef2cc3
Aman Kumar <amansinha327@gmail.com>
2025-12-01 15:37:24 +0000

    Added WalletContext and wrap App with providers

--- frontend/src/contexts/WalletContext.tsx
+++ frontend/src/contexts/WalletContext.tsx
@@ -1,4 +1,5 @@
-import { FC, ReactNode, useMemo } from "react";
+import type { FC, ReactNode } from "react";
+import { useMemo } from "react";
 import {
     ConnectionProvider,
     WalletProvider
@@ -7,23 +8,19 @@ import {
 import {
     PhantomWalletAdapter,
     SolflareWalletAdapter,
-    BackpackWalletAdapter,
 } from "@solana/wallet-adapter-wallets";
 
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 
 import("@solana/wallet-adapter-react-ui/styles.css");
 
 export const WalletContext: FC<{ children: ReactNode }> = ({ children }) => {
-    const network = WalletAdapterNetwork.Devnet;
     const endpoint = "https://api.devnet.solana.com";
 
     const wallets = useMemo(
         () => [
             new PhantomWalletAdapter(),
             new SolflareWalletAdapter(),
-            new BackpackWalletAdapter(),
         ],
         []
     );
